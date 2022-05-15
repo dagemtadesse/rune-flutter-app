cdf1799c9087388921be670ed119c1b07a36ca3e
naomitimm <91492667+naomitimm@users.noreply.github.com>
2022-05-15 05:04:00 +0300

    Expanded widget fix

--- rune/lib/widgets/channel_details_page.dart
+++ rune/lib/widgets/channel_details_page.dart
@@ -72,158 +72,151 @@ class _ChannelDetailsPageState extends State<ChannelDetailsPage> {
     return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 60.0),
         child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(
-                  color: Color.fromARGB(255, 140, 139, 139), width: 1.0),
-            ),
-            constraints: const BoxConstraints.expand(height: 450),
-            child: Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(
+                color: Color.fromARGB(255, 140, 139, 139), width: 1.0),
+          ),
+          constraints: const BoxConstraints.expand(height: 450),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        child: Padding(
-                          padding:
-                              const EdgeInsets.fromLTRB(25.0, 15.0, 25.0, 10.0),
-                          child: Row(
-                            children: [
-                              CircleAvatar(
-                                radius: 40,
-                                backgroundImage: AssetImage("assets/test.jpg"),
-                              ),
-                              SizedBox(
-                                width: 20,
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text("Publisher Name"),
-                                  Text("Jan 20 2021")
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.more_vert),
-                        color: Colors.black,
-                        iconSize: 25.0,
-                      )
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                    child: Text(
-                      "Post Title",
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
                   Container(
-                    constraints: BoxConstraints.expand(
-                        height: 200, width: double.infinity),
-                    child: Image(
-                      image: AssetImage("assets/test.jpg"),
-                      fit: BoxFit.cover,
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.fromLTRB(25.0, 15.0, 25.0, 10.0),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 40,
+                            backgroundImage: AssetImage("assets/test.jpg"),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Publisher Name"),
+                              Text("Jan 20 2021")
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(40.0, 10.0, 40.0, 0.0),
-                    child: Text(
-                      "This is the random post content apparently wow wow wow wow ",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  Expanded(child: PostBottomBtns())
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.more_vert),
+                    color: Colors.black,
+                    iconSize: 25.0,
+                  )
                 ],
               ),
-            )));
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                child: Text(
+                  "Post Title",
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                constraints:
+                    BoxConstraints.expand(height: 200, width: double.infinity),
+                child: Image(
+                  image: AssetImage("assets/test.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(40.0, 10.0, 40.0, 0.0),
+                child: Text(
+                  "This is the random post content apparently wow wow wow wow ",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              PostBottomBtns()
+            ],
+          ),
+        ));
   }
 
   Widget UserPost() {
     return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 60.0),
-        child: Expanded(
-          child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(
-                    color: Color.fromARGB(255, 140, 139, 139), width: 1.0),
-              ),
-              constraints: const BoxConstraints.expand(height: 400),
-              child: Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(
-                                25.0, 15.0, 25.0, 10.0),
-                            child: Row(
-                              children: [
-                                CircleAvatar(
-                                  radius: 40,
-                                  backgroundImage:
-                                      AssetImage("assets/test.jpg"),
-                                ),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("Publisher Name"),
-                                    Text("Jan 20 2021")
-                                  ],
-                                )
-                              ],
-                            ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(
+                color: Color.fromARGB(255, 140, 139, 139), width: 1.0),
+          ),
+          constraints: const BoxConstraints.expand(height: 400),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.fromLTRB(25.0, 15.0, 25.0, 10.0),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 40,
+                            backgroundImage: AssetImage("assets/test.jpg"),
                           ),
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.more_vert),
-                          color: Colors.black,
-                          iconSize: 25.0,
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                      child: Text(
-                        "Post Title",
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Publisher Name"),
+                              Text("Jan 20 2021")
+                            ],
+                          )
+                        ],
                       ),
                     ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                      child: Container(
-                        child: Text(
-                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit dolor a sagittis ornare. Aenean vestibulum eros a lorem tristique, consectetur lobortis mi aliquam. Nulla condimentum in leo sed pretium. Integer vitae magna sit amet libero commodo fermentum. Maecenas semper justo vel tempus efficitur. Donec feugiat ultrices odio, at varius ligula accumsan quis. Duis in porttitor massa. Aliquam erat volutpat. Cras lorem sem, molestie quis quam ac, semper rhoncus tortor. In justo dolor, ultrices sed nunc at, ornare imperdiet sapien. Nunc non mi laoreet, tempus sem a, aliquam enim.",
-                          style: TextStyle(fontSize: 16.0),
-                        ),
-                      ),
-                    ),
-                    Expanded(child: PostBottomBtns()),
-                  ],
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.more_vert),
+                    color: Colors.black,
+                    iconSize: 25.0,
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                child: Text(
+                  "Post Title",
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
                 ),
-              )),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                child: Container(
+                  child: Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit dolor a sagittis ornare. Aenean vestibulum eros a lorem tristique, consectetur lobortis mi aliquam. Nulla condimentum in leo sed pretium. Integer vitae magna sit amet libero commodo fermentum. Maecenas semper justo vel tempus efficitur. Donec feugiat ultrices odio, at varius ligula accumsan quis. Duis in porttitor massa. Aliquam erat volutpat. Cras lorem sem, molestie quis quam ac, semper rhoncus tortor. In justo dolor, ultrices sed nunc at, ornare imperdiet sapien. Nunc non mi laoreet, tempus sem a, aliquam enim.",
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ),
+              ),
+              PostBottomBtns(),
+            ],
+          ),
         ));
   }
 
@@ -280,6 +273,9 @@ class _ChannelDetailsPageState extends State<ChannelDetailsPage> {
             height: 30.0,
           ),
           UserPost(),
+          const SizedBox(
+            height: 30.0,
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
