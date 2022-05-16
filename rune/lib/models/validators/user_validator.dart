c7fcf19c2b7c741427faa8e21a106b6c83902885
altair <74534830+yeabsra-leul@users.noreply.github.com>
2022-05-16 19:55:49 +0300

    Implemented validation

--- rune/lib/models/validators/user_validator.dart
+++ rune/lib/models/validators/user_validator.dart
@@ -1,21 +1,58 @@
 class UserValidator {
+
   static String? validateFullName(String fullName) {
-    throw UnimplementedError();
+    if (fullName == null || fullName.trim().isEmpty) {
+      return "Please enter your full name";
+    }
+    if (!RegExp(r'^[a-z A-Z]+$').hasMatch(fullName)) {
+      return "Please Enter a correct name";
+    } else {
+      return null;
+    }
   }
 
   static String? validateEmail(String email) {
-    throw UnimplementedError();
+    if (email == null || email.trim().isEmpty) {
+      return 'Please enter your email address';
+    }
+
+    if (!RegExp(r'\S+@\S+\.\S+').hasMatch(email)) {
+      return 'Please enter a valid email address';
+    }
+
+    return null;
   }
 
   static String? validatePassword(String password) {
-    throw UnimplementedError();
+    if (password == null || password.trim().isEmpty) {
+      return 'This field is required';
+    }
+    if (password.trim().length < 8) {
+      return 'Password must be at least 8 characters in length';
+    }
+    return null;
   }
 
-  static String? validateConfirmPassword(String password, String cPassword) {
-    throw UnimplementedError();
+  static String? validateConfirmPassword(
+      String cPassword, String password) {
+    if (password == null || password.isEmpty) {
+      return 'This field is required';
+    }
+
+    if (password != cPassword) {
+      return 'Confimation password does not match the entered password';
+    }
+
+    return null;
   }
 
   static String? validateUserName(String userName) {
-    throw UnimplementedError();
+    if (userName == null || userName.trim().isEmpty) {
+      return "Username can't be empty";
+    }
+    if (userName.trim().length < 4) {
+      return 'Username must be at least 4 characters in length';
+    }
+    return null;
   }
 }
