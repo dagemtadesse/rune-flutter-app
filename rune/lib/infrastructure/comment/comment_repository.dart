9d05256ed410c5794fd3e7570ea9c9da119574ea
altair <74534830+yeabsra-leul@users.noreply.github.com>
2022-06-03 17:28:42 +0300

    Update comment_repository.dart

--- rune/lib/infrastructure/comment/comment_repository.dart
+++ rune/lib/infrastructure/comment/comment_repository.dart
@@ -1,1 +1,43 @@
-class CommentRepository {}
+import '../../domain/comment/comment_model.dart';
+import '../../domain/user/user_model.dart';
+import 'comment_api_provider.dart';
+
+class CommentRepository {
+ final CommentAPIProvider dataProvider;
+ CommentRepository(this.dataProvider);
+
+ Future<List<Comment>> fetchComments(User user,int postId ) async {
+   return dataProvider.fetchComments(user,postId);
+ }
+
+Future<Comment> fetchComment(User user,int commentId ) async {
+  return dataProvider.fetchComment(user,commentId);
+}
+
+Future<Comment> createComment(User user,int postId , String content) async {
+  return dataProvider.createComment(user,postId,content);
+}
+
+Future<Comment> delete(User user,int commentId ) async {
+  return dataProvider.delete(user,commentId);
+}
+
+Future<Comment> update(User user,int commentId , String content) async {
+  return dataProvider.update(user,commentId,content);
+}
+
+Future<Comment> vote(User user,int commentId ) async {
+  return dataProvider.vote(user,commentId);
+}
+
+
+
+
+
+
+
+
+
+}
+
+
