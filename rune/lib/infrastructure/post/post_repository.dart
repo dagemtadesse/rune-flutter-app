0882e616af84f365ebb2f748745dfb2b562257b7
altair <74534830+yeabsra-leul@users.noreply.github.com>
2022-06-03 17:27:58 +0300

    Update post_repository.dart

--- rune/lib/infrastructure/post/post_repository.dart
+++ rune/lib/infrastructure/post/post_repository.dart
@@ -1,1 +1,34 @@
-class PostRepository {}
+import '../../domain/post/post_model.dart';
+import '../../domain/user/user_model.dart';
+import 'post_api_provider.dart';
+
+class PostRepository {
+  final PostAPIProvider dataProvider;
+  PostRepository(this.dataProvider);
+
+  Future<List<Post>> fetchPosts(User user, int channelId) async {
+    return dataProvider.fetchPosts(user, channelId);
+  }
+
+  Future<Post> fetchPost(User user, int postId) async {
+    return dataProvider.fetchPost(user, postId);
+  }
+
+  Future<Post> createPost(
+      User user, int channelId, String content, String title) async {
+    return dataProvider.createPost(user, channelId,
+        content: 'Null', title: 'Null');
+  }
+
+  Future<Post> delete(User user, int postId) async {
+    return dataProvider.delete(user, postId);
+  }
+
+  Future<Post> update(User user, int postID, String content) async {
+    return dataProvider.update(user, postID, content: "Null");
+  }
+
+  Future<Post> vote(User user, int commentId) async {
+    return dataProvider.vote(user, commentId);
+  }
+}
