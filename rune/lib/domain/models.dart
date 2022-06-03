3927e5e4da6a18a0dae45cc0b5f979ba5a680ca9
naomitimm <91492667+naomitimm@users.noreply.github.com>
2022-06-03 11:06:18 +0300

    splash overflow fix

--- rune/lib/domain/models.dart
+++ rune/lib/domain/models.dart
@@ -1,4 +1,4 @@
-export 'user/user.dart';
+export 'user/user_model.dart';
 export 'channel/channel_model.dart';
 export 'post/post_model.dart';
 export 'comment/comment_model.dart';
--- rune/lib/presentation/splash/splash_screen.dart
+++ rune/lib/presentation/splash/splash_screen.dart
@@ -7,90 +7,87 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Flexible(
-            flex: 42,
-            child: Container(
-              padding: const EdgeInsets.all(18.0),
-              child: const Image(
-                image: AssetImage("assets/splash.jpg"),
-                fit: BoxFit.fill,
-              ),
+          Container(
+            padding: const EdgeInsets.all(18.0),
+            child: const Image(
+              image: AssetImage("assets/splash.jpg"),
+              fit: BoxFit.fill,
             ),
           ),
-          Flexible(
-            flex: 58,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 18.0),
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/waves.png"),
-                    fit: BoxFit.cover,
-                    alignment: Alignment.topLeft),
-              ),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: Column(
-                      children: [
-                        const SizedBox(
-                          height: 100,
-                        ),
-                        Text("Rune", style: SplashTheme.textTheme.headline1),
-                        const SizedBox(
-                          height: 24,
-                        ),
-                        Text(
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/waves.png"),
+                  fit: BoxFit.cover,
+                  alignment: Alignment.topLeft),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 100,
+                      ),
+                      Text("Rune", style: SplashTheme.textTheme.headline1),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      Container(
+                        child: Text(
                           "lorem iosum do  dor sit amet id the simple lorem iosum  dor sit amet id the simple iosum  id the simple iosum   dor sit amet id the simple lorem iosum dodor sit amet id the simplevvv sample paragraph",
                           style: SplashTheme.textTheme.headline3,
                           textAlign: TextAlign.center,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(vertical: 24.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      color: const Color.fromRGBO(228, 228, 220, 1),
-                    ),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (_) => SignUpScreen()));
-                            },
-                            child: Text("Register",
-                                style: SplashTheme.textTheme.labelMedium),
-                            style: SplashTheme.buttonTheme,
-                          ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 24.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    color: const Color.fromRGBO(228, 228, 220, 1),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) => SignUpScreen()));
+                          },
+                          child: Text("Register",
+                              style: SplashTheme.textTheme.labelMedium),
+                          style: SplashTheme.buttonTheme,
                         ),
-                        Expanded(
-                          child: TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (_) => SignInScreen()));
-                            },
-                            child: Text("Sign In",
-                                style: SplashTheme.textTheme.labelMedium),
-                            style: SplashTheme.transparentButton,
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                      ),
+                      Expanded(
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) => SignInScreen()));
+                          },
+                          child: Text("Sign In",
+                              style: SplashTheme.textTheme.labelMedium),
+                          style: SplashTheme.transparentButton,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ],
