26dce3355f881c72d16f193cb7dbc30e921b1211
altair <74534830+yeabsra-leul@users.noreply.github.com>
2022-06-05 01:13:26 +0300

    Update channel_state.dart

--- rune/lib/application/channel/bloc/channel_state.dart
+++ rune/lib/application/channel/bloc/channel_state.dart
@@ -28,4 +28,26 @@ class ChannelCreated extends ChannelState {}
 
 class ChannelDeleted extends ChannelState {}
 
-class SearchSuccessful extends ChannelState {}
+abstract class SearchState extends ChannelState {
+  const SearchState();
+
+  @override
+  List<Object> get props => [];
+
+}
+
+class SearchLoading extends SearchState {}
+
+class SearchLoadedSuccesfully extends SearchState{
+  final List<Channel> channels;
+
+  const SearchLoadedSuccesfully({required this.channels});
+  @override
+  List<Object> get props => [channels];
+}
+
+class SearchLoadingFailed extends SearchState {
+  final String errMsg;
+  const SearchLoadingFailed(this.errMsg);
+}
+
