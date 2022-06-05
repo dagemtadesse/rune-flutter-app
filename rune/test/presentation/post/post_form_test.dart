d5766ef11482f15dcfdc9f5f084402a72ab22131
Dagem Tadesse <dagem.seyfu@gmail.com>
2022-06-05 23:13:05 +0300

    debug widget test

--- rune/test/presentation/post/post_form_test.dart
+++ rune/test/presentation/post/post_form_test.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:rune/domain/models.dart';
 import 'package:rune/presentation/post/post_form.dart';
 import 'package:rune/presentation/post/widgets/post_input.dart';
 
-void main(){
+void main() {
   testWidgets('Finding and verifying Widgets in Post Form page',
       (WidgetTester tester) async {
-    await tester.pumpWidget(PostForm());
+    final myChannel = Channel(
+        id: 0,
+        authorId: 0,
+        name: 'Dagem',
+        description: 'description',
+        verified: false,
+        updatedAt: DateTime.now(),
+        pinned: false);
+
+    await tester.pumpWidget(PostForm(
+        formKey: GlobalKey<FormState>(),
+        titleController: TextEditingController(),
+        contentController: TextEditingController(),
+        channel: myChannel));
 
     final containerWidgetCount = find.byType(Container);
-    final scrollerWidgetCount= find.byType(SingleChildScrollView);
+    final scrollerWidgetCount = find.byType(SingleChildScrollView);
 
     final formWidgetCount = find.byType(Form);
-    final inputFiedCount= find.byType(PostInput);
+    final inputFiedCount = find.byType(PostInput);
 
-    final dividerWidgetCount= find.byType(Divider);
+    final dividerWidgetCount = find.byType(Divider);
 
     expect(containerWidgetCount, findsOneWidget);
     expect(scrollerWidgetCount, findsOneWidget);
@@ -23,9 +37,5 @@ void main(){
     expect(inputFiedCount, findsNWidgets(2));
 
     expect(dividerWidgetCount, findsOneWidget);
-
-
-
-
   });
 }
